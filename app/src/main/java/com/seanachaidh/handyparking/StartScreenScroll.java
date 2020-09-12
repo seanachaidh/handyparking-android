997a48bb35e0e0a3d92b3381bbe81e372876e6fa
Pieter Van Keymeulen <pvankeymeulen@seanachaidh.be>
2020-09-12 19:08:39 +0200

    ScrollView startscreen toevoegen

---
+++ app/src/main/java/com/seanachaidh/handyparking/StartScreenScroll.java
@@ -0,0 +1,23 @@
+package com.seanachaidh.handyparking;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.widget.ScrollView;
+
+public class StartScreenScroll extends ScrollView {
+    public StartScreenScroll(Context context) {
+        super(context);
+    }
+
+    public StartScreenScroll(Context context, AttributeSet attrs) {
+        super(context, attrs);
+    }
+
+    public StartScreenScroll(Context context, AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+    }
+
+    public StartScreenScroll(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
+        super(context, attrs, defStyleAttr, defStyleRes);
+    }
+}
