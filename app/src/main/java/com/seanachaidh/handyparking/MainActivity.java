d4c416b5fd6991a3bf66fb31ea680c79ade7b050
Pieter Van Keymeulen <pvankeymeulen@seanachaidh.be>
2020-09-17 09:19:55 +0200

    veiligheidscommit

--- app/src/main/java/com/seanachaidh/handyparking/MainActivity.java
+++ app/src/main/java/com/seanachaidh/handyparking/MainActivity.java
@@ -1,9 +1,5 @@
 package com.seanachaidh.handyparking;
 
-import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.app.ActivityCompat;
-
 import android.Manifest;
 import android.content.Context;
 import android.content.pm.PackageManager;
@@ -11,15 +7,24 @@ import android.location.Location;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.util.Log;
-import android.webkit.WebSettings;
-import android.webkit.WebView;
+import android.view.View;
+import android.widget.ScrollView;
 
-import java.io.BufferedReader;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.stream.Collectors;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ScrollView login_view;
+    private ScrollView register_view;
+    private StartScreenScroll start_screen_scroll;
+
+    private void showScrollView() {
+        if(this.start_screen_scroll.getVisibility() == View.INVISIBLE) {
+            this.start_screen_scroll.setVisibility(View.VISIBLE);
+        }
+    }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
@@ -45,11 +50,21 @@ public class MainActivity extends AppCompatActivity {
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_COARSE_LOCATION}, 1);
         }
-        LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
-        Location loc = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
 
-        Log.d("general", loc.toString());
+        Log.d("general", "loading extra views");
 
         setContentView(R.layout.activity_main);
+        this.start_screen_scroll = (StartScreenScroll) findViewById(R.id.login_scroll);
+    }
+
+    public void showLogin(View view) {
+        showScrollView();
+        this.start_screen_scroll.useLogin();
+
+    }
+
+    public void showRegister(View view) {
+        showScrollView();
+        this.start_screen_scroll.useRegister();
     }
 }
--- app/src/main/java/com/seanachaidh/handyparking/StartScreenScroll.java
+++ app/src/main/java/com/seanachaidh/handyparking/StartScreenScroll.java
@@ -1,23 +1,116 @@
 package com.seanachaidh.handyparking;
 
 import android.content.Context;
+import android.text.Layout;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
 import android.widget.ScrollView;
 
+import androidx.appcompat.widget.LinearLayoutCompat;
+
 public class StartScreenScroll extends ScrollView {
+
+    private class RegisterOnClick implements OnClickListener {
+
+        @Override
+        public void onClick(View view) {
+            Log.d("clickbuttons", "Register has been clicked");
+            LinearLayoutCompat scroll = (LinearLayoutCompat) view.getParent();
+
+            EditText name = (EditText) scroll.findViewById(R.id.register_name_text);
+            EditText email = (EditText) scroll.findViewById(R.id.register_email_text);
+            EditText password = (EditText) scroll.findViewById(R.id.register_password_text);
+            CheckBox guide = (CheckBox) scroll.findViewById(R.id.register_guide_check);
+
+            
+
+        }
+    }
+
+    private class LoginOnClick implements OnClickListener {
+
+        @Override
+        public void onClick(View view) {
+            Log.d("clickbuttons", "Login has been clicked");
+        }
+    }
+
+    private LinearLayoutCompat login_view;
+    private LinearLayoutCompat register_view;
+
+    private Button register_confirmation_button;
+    private Button login_confirmation_button;
+
+
+    private Boolean use_login = false;
+    public void switchLayout() {
+        this.removeAllViews();
+        if(use_login) {
+            this.addView(this.register_view);
+        } else {
+            this.addView(this.login_view);
+        }
+        this.use_login = !this.use_login;
+    }
+
+    public void useLogin() {
+        if(!use_login) {
+            this.removeAllViews();
+            this.addView(login_view);
+            this.use_login = true;
+        }
+    }
+
+    public void useRegister() {
+        if(use_login) {
+            this.removeAllViews();
+            this.addView(register_view);
+            this.use_login = false;
+        }
+    }
+
+    private void createViews() {
+        LayoutInflater inflater = (LayoutInflater) getContext().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+        this.login_view = (LinearLayoutCompat) inflater.inflate(R.layout.login_layout, null);
+        this.register_view = (LinearLayoutCompat) inflater.inflate(R.layout.register_layout, null);
+
+        this.register_confirmation_button = this.register_view.findViewById(R.id.register_confirmation_button);
+        this.register_confirmation_button.setOnClickListener(new RegisterOnClick());
+
+        this.login_confirmation_button = this.login_view.findViewById(R.id.login_confirmation_button);
+        this.login_confirmation_button.setOnClickListener(new LoginOnClick());
+
+        //we beginnen met register
+        this.addView(this.register_view);
+    }
+
     public StartScreenScroll(Context context) {
         super(context);
+        createViews();
     }
 
     public StartScreenScroll(Context context, AttributeSet attrs) {
         super(context, attrs);
+        createViews();
     }
 
     public StartScreenScroll(Context context, AttributeSet attrs, int defStyleAttr) {
         super(context, attrs, defStyleAttr);
+        createViews();
     }
 
     public StartScreenScroll(Context context, AttributeSet attrs, int defStyleAttr, int defStyleRes) {
         super(context, attrs, defStyleAttr, defStyleRes);
+        createViews();
     }
+
+    public void confirmRegister(View view) {
+        Log.d("general", "Register confirmation");
+    }
+
 }
