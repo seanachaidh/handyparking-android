ad8cd8c3e6ae751dc97c036471ec5ba8afe8cacf
Pieter Van Keymeulen <pvankeymeulen@seanachaidh.be>
2020-09-18 18:31:25 +0200

    Implementeren singleton

--- app/src/main/java/com/seanachaidh/handyandroid/ClientSingleton.java
+++ app/src/main/java/com/seanachaidh/handyandroid/ClientSingleton.java
@@ -1,4 +1,27 @@
 package com.seanachaidh.handyandroid;
 
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+import org.apache.hc.client5.http.impl.classic.HttpClients;
+
 public class ClientSingleton {
+
+    private static ClientSingleton instance = null;
+
+    private CloseableHttpClient client;
+
+    private ClientSingleton() {
+        this.client = HttpClients.createDefault();
+    }
+
+    public CloseableHttpClient getClient() {
+        return client;
+    }
+
+    public static ClientSingleton getInstance() {
+        if(instance == null) {
+            instance = new ClientSingleton();
+        }
+
+        return instance;
+    }
 }
