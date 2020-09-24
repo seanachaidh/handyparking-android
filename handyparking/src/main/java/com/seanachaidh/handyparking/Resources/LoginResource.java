ca0a1d351fec1876eaa478ca2153f7b4156e7c68
Pieter Van Keymeulen <pvankeymeulen@seanachaidh.be>
2020-09-24 09:35:39 +0200

    implementing loginresource

--- handyparking/src/main/java/com/seanachaidh/handyparking/Resources/LoginResource.java
+++ handyparking/src/main/java/com/seanachaidh/handyparking/Resources/LoginResource.java
@@ -1,4 +1,13 @@
 package com.seanachaidh.handyparking.Resources;
 
-public class LoginResource {
+import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
+
+public class LoginResource extends Resource<LoginResource.LoginResult> {
+    public LoginResource(CloseableHttpClient client) {
+        super(LoginResource.LoginResult[].class, "/login", client);
+    }
+
+    public static class LoginResult {
+        String token;
+    }
 }
